1bef4d038afcf7f4677891f4a577b21e3ea987f9
mailbak36@gmail.com <thinkthank001>
2018-09-25 15:28:06 +0500

    base fragment

---
+++ app/src/main/java/com/jim/multipos/core/TabbedFragment.kt
@@ -0,0 +1,83 @@
+package com.jim.multipos.core
+
+import android.databinding.ViewDataBinding
+import android.graphics.drawable.GradientDrawable
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.LinearLayout
+import com.jim.multipos.R
+import kotlinx.android.synthetic.main.tabbed_fragment.*
+
+abstract class TabbedFragment<T: ViewDataBinding, V: BaseViewModel>: BaseFragment<T, V>() {
+
+
+    companion object {
+        val TAB_FRAGMENT_TAG = "TAB_FRAGMENT_TAG"
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (tlContainer.tabCount == 0) {
+
+            val data = getTabData()
+            if (data.isNotEmpty()) {
+
+                for (key in data.keys) {
+                    tlContainer.addTab(tlContainer.newTab().setText(key))
+                }
+                val tab = tlContainer.getChildAt(0)
+                (tab as LinearLayout).showDividers = LinearLayout.SHOW_DIVIDER_MIDDLE
+                val drawable = GradientDrawable()
+                drawable.setColor(ContextCompat.getColor(context!!, R.color.colorGray))
+                drawable.setSize(1, 1)
+                tab.dividerDrawable = drawable
+                tlContainer.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                    override fun onTabReselected(tab: TabLayout.Tab?) {}
+                    override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                    override fun onTabSelected(tab: TabLayout.Tab?) {
+                        if (tab?.text != null && data.get(tab.text) != null) {
+                            openFragment(data.get(tab.text)!!)
+                        }
+                    }
+
+                })
+                val defaultTabName = getDefaultTabName()
+                if (defaultTabName != null) {
+                    if (data[defaultTabName] != null) {
+                        openFragment(data[defaultTabName]!!)
+                    }
+                } else {
+                    if (data[data.keys.toList()[0]] != null) {
+                        openFragment(data[data.keys.toList()[0]]!!)
+                    }
+                }
+            } else {
+                throw Exception(message = "Tab data is not set!!!")
+            }
+        }
+
+    }
+
+
+    private fun openFragment(fragment: Fragment) {
+        activity
+                ?.supportFragmentManager
+                ?.beginTransaction()
+                ?.replace(R.id.flTabContainer, fragment, TAB_FRAGMENT_TAG)
+                ?.commitNow()
+    }
+
+
+    override fun getLayoutId(): Int {
+        return R.layout.tabbed_fragment
+    }
+
+    abstract fun getTabData(): Map<String, Fragment>
+
+    fun getDefaultTabName(): String? = null
+
+}
---
+++ app/src/main/java/com/jim/multipos/core/TrippleHorizontalFragment.kt
@@ -0,0 +1,64 @@
+package com.jim.multipos.core
+
+import android.databinding.ViewDataBinding
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import com.jim.multipos.R
+import kotlinx.android.synthetic.main.search_header_fragment.*
+
+abstract class TrippleHorizontalFragment<T: ViewDataBinding, V: BaseViewModel>: SearchHeaderFragment<T, V>() {
+
+
+
+
+    companion object {
+        protected val LEFT_FRAGMENT_TAG = "LEFT_FRAGMENT_TAG"
+        protected val CENTER_FRAGMENT_TAG = "LEFT_FRAGMENT_TAG"
+        protected val RIGHT_FRAGMENT_TAG = "RIGHT_FRAGMENT_TAG"
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (getLeftFragment() == null) {
+            throw Exception(message = "LEFT Fragment is not set")
+        }
+        if (getRightFragment() == null) {
+            throw Exception(message = "RIGHT Fragment is not set")
+        }
+
+        if (activity?.supportFragmentManager?.findFragmentByTag(LEFT_FRAGMENT_TAG) == null &&
+                activity?.supportFragmentManager?.findFragmentByTag(CENTER_FRAGMENT_TAG) == null &&
+                activity?.supportFragmentManager?.findFragmentByTag(RIGHT_FRAGMENT_TAG) == null) {
+
+            initFragmentContent()
+
+            activity
+                    ?.supportFragmentManager
+                    ?.beginTransaction()
+                    ?.add(R.id.flLeftConainer, getLeftFragment(), LEFT_FRAGMENT_TAG)
+                    ?.commit()
+
+            activity
+                    ?.supportFragmentManager
+                    ?.beginTransaction()
+                    ?.add(R.id.flCenterContainer, getCenterFragment(), CENTER_FRAGMENT_TAG)
+                    ?.commit()
+
+            activity
+                    ?.supportFragmentManager
+                    ?.beginTransaction()
+                    ?.add(R.id.flRightConainer, getRightFragment(), RIGHT_FRAGMENT_TAG)
+                    ?.commit()
+        }
+    }
+
+    abstract fun getLeftFragment() : Fragment?
+    abstract fun getCenterFragment() : Fragment?
+    abstract fun getRightFragment() : Fragment?
+
+    private fun initFragmentContent() {
+        LayoutInflater.from(context).inflate(R.layout.tripple_horizontal_fragment, flContainer, true)
+    }
+}
