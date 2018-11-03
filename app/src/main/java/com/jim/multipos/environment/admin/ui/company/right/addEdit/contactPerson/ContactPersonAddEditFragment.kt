65b5ac633b7259b047c351774aa0f8252cce629c
developer <mailbak36@gmail.com>
2018-11-03 15:29:00 +0500

    company add edit

--- app/src/main/java/com/jim/multipos/environment/admin/ui/company/right/addEdit/contactPerson/ContactPersonAddEditFragment.kt
+++ app/src/main/java/com/jim/multipos/environment/admin/ui/company/right/addEdit/contactPerson/ContactPersonAddEditFragment.kt
@@ -7,31 +7,29 @@ import com.jim.multipos.BR
 import com.jim.multipos.R
 import com.jim.multipos.core.ViewModelProviderFactory
 import com.jim.multipos.core.fragments.BaseFragment
-import com.jim.multipos.databinding.CompanyContactPersonFragmentBinding
+import com.jim.multipos.databinding.CompanyContactPersonAddEditLayoutBinding
 import javax.inject.Inject
 
-class ContactPersonFragment: BaseFragment<CompanyContactPersonFragmentBinding, ContactPersonViewModel>() {
+class ContactPersonAddEditFragment: BaseFragment<CompanyContactPersonAddEditLayoutBinding, ContactPersonAddEditViewModel>() {
+
 
     @Inject
     lateinit var factory: ViewModelProviderFactory
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
 
-        mViewModel?.onViewCreated()
-        this.initObservers()
     }
-
-    private fun initObservers() {}
 
     override fun getBindingVariable(): Int = BR.viewModel
-    override fun getLayoutId(): Int = R.layout.company_contact_person_fragment
-    override fun getViewModel(): ContactPersonViewModel {
-        mViewModel = ViewModelProviders.of(this, factory).get(ContactPersonViewModel::class.java)
-        return mViewModel as ContactPersonViewModel
+    override fun getLayoutId(): Int = R.layout.company_contact_person_add_edit_layout
+    override fun getViewModel(): ContactPersonAddEditViewModel {
+        mViewModel = ViewModelProviders.of(this, factory)[ContactPersonAddEditViewModel::class.java]
+        return mViewModel as ContactPersonAddEditViewModel
     }
 
-    fun deliverDataToMainClass() {}
+
 }
---
+++ app/src/main/java/com/jim/multipos/environment/admin/ui/company/right/addEdit/contactPerson/ContactPersonAddEditViewModel.kt
@@ -0,0 +1,9 @@
+package com.jim.multipos.environment.admin.ui.company.right.addEdit.contactPerson
+
+import com.jim.multipos.core.BaseViewModel
+import com.jim.multipos.core.managers.DataManager
+import javax.inject.Inject
+
+class ContactPersonAddEditViewModel @Inject constructor(dataManager: DataManager): BaseViewModel(dataManager) {
+
+}
